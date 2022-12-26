b42ee1318efcda37a5798c2fabf5f8e30ad4b2fb
Shaikhaltamash861 <altamash2c2@gmail.com>
2022-12-26 17:46:38 +0530

    socket first

---
+++ index.js
@@ -0,0 +1,42 @@
+const PORT=process.env.PORT||8900;
+const io=require('socket.io')(PORT,{
+    cors:{
+        origin:'*'
+    }
+})
+let users=[]
+const addUsers=(userId,socketId)=>{
+    
+    !users.some((user)=>user.userId===userId)&&
+    users.push({userId,socketId})
+}
+const removeUser=(socketId)=>{
+    users=users.filter((user)=>user.socketId!==socketId)
+    
+}
+const getUsers=(userId)=>{
+      return users.find((user)=>user.userId==userId)
+}
+io.on('connection',(socket)=>{
+    // console.log(users)
+    socket.on('addUsers',(userId)=>{
+        
+        addUsers(userId,socket.id)
+        io.emit('getUsers',users)
+    })
+    socket.on('sendMessage',({senderId,receiverId,text})=>{
+        
+        const user=getUsers(receiverId);
+        if( user?.socketId){
+
+            io.to(user.socketId).emit('getMessage',{senderId,text})
+        }
+        
+    })
+    socket.on('disconnect',()=>{
+        console.log('user is disconnected')
+        
+        removeUser(socket.id)
+        io.emit('getUsers',users)
+    })
+})
